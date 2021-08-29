1c166e9d867277e14b5b55289290e88eede65cfc
NamrathaHV <1rn18cs067.namratha.h.v@gmail.com>
2021-08-29 15:18:14 +0530

    Final Stage Done!

--- app/src/main/java/com/example/unit_converter/adapters/MoreAdapter.java
+++ app/src/main/java/com/example/unit_converter/adapters/MoreAdapter.java
@@ -57,7 +57,7 @@ public class MoreAdapter extends RecyclerView.Adapter<MoreAdapter.ViewHolder> {
             switch (item.getId()) {
 
                 case "support_feedback":
-                    String EMAIL = "1rn18cs067.namratha.h.v@gmail.com";
+                    String EMAIL = "namrathahv727@gmail.com";
                     String[] emailAddress = {EMAIL};
                     Intent supportFeedback = new Intent(Intent.ACTION_SENDTO);
                     supportFeedback.setData(Uri.parse("mailto:"));
